380e2ac3ca16c6ecae1b561798490f837aecd65d
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-01 10:32:52 +0100

    Javadoc for the PGM class

--- src/pgm/baby/PGM.java
+++ src/pgm/baby/PGM.java
@@ -9,6 +9,13 @@ public class PGM {
     private int maxVal;
     private int[][] pixelValues;
 
+    /**
+     * Constructor for a PGM
+     * @param width number of colomns for the matrix
+     * @param height number of rows for the matrix
+     * @param maxVal Threshold for white value, give the number of grayscale available
+     * @param pixelValues Matrix to initialize a new PGM
+     */
     public PGM(int width, int height, int maxVal, int[][] pixelValues) {
         this.width = width;
         this.height = height;
@@ -16,6 +23,10 @@ public class PGM {
         this.pixelValues = pixelValues;
     }
 
+    /**
+     * Make an histogramm from the current PGM
+     * @return number of occurences of each grayscale between 0 and maxVal-1
+     */
     public int[] histogram() {
         int[] hist = new int[this.maxVal];
         for (int[] row : pixelValues) {
@@ -26,6 +37,11 @@ public class PGM {
         return hist;
     }
 
+    /**
+     * Apply a threshold filter on a opened PGM file
+     * @param thresh affect the threshold for comparison
+     * @return black and white PGM file thresholded
+     */
     public PGM threshold(int thresh) {
         int[][] newPixelValues = new int[height][width];
         for (int i = 0; i < height; i++) {
@@ -38,6 +54,11 @@ public class PGM {
         return new PGM(width, height, maxVal, newPixelValues);
     }
 
+    /**
+     * Allow minus comparison between two PGM files with the same size and maxVal
+     * @param that other PGM file to compare with the first one
+     * @return PGM file to show minus differences
+     */
     public PGM minus(PGM that) {
         if (height != that.height || width != that.width || maxVal != that.maxVal) {
             throw new UnsupportedOperationException("image parameters must match");
