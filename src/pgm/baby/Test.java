f015005c820aaf9ae7737cf1c5755e3932c7f8f3
Thomas Buick <zogwarg@gmail.com>
2014-12-01 14:11:57 +0100

    Rewrite Test

--- src/pgm/baby/Test.java
+++ src/pgm/baby/Test.java
@@ -5,7 +5,22 @@ package pgm.baby;
  */
 public class Test {
     public static void main(String[] args) {
-        Loader loader = new Loader("feep.pgm");
         GUI gui = new GUI(640, 320);
+        PGM pgm = null;
+        try {
+            System.out.println("Opening file obama.pgm");
+            Loader loader = new Loader("obama.pgm");
+            System.out.println("Loading file into PGM object");
+            pgm = loader.loadPGM();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        if (pgm != null) {
+            System.out.println("Saving Histogram as PGM");
+            pgm.histogram().save("histogram.pgm");
+        }
+
+        System.out.println("Done.");
     }
 }
