a0f4f900a97852234e9c4eeed827a7818f659e1c
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-01 14:28:57 +0100

    Add parenthesis to prevent bug for caracter limit

--- src/pgm/core/PGM.java
+++ src/pgm/core/PGM.java
@@ -216,7 +216,7 @@ public class PGM {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 out += Integer.toString(pixelValues[i][j]) + " ";
-                if (i+j%17 == 16) { // Only 17 values by lines, to be sure we cannot have more than 70 caracters by lines
+                if ((i+j)%17 == 16) { // Only 17 values by lines, to be sure we cannot have more than 70 caracters by lines
                     out += "\n";
                 }
             }
