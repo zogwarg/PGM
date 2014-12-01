1ae7861a1682f88f31c99f87116cb9a49b093875
Thomas Buick <zogwarg@gmail.com>
2014-12-01 10:09:49 +0100

    Cleaning File name

--- src/pgm/baby/GUI.java
+++ src/pgm/baby/GUI.java
@@ -3,7 +3,7 @@ package pgm.baby;
 /**
  * Created by Thomas on 12/1/2014.
  */
-public class Lobster {
+public class GUI {
     public static void main(String[] args) {
         Loader loader = new Loader("Test.txt");
     }
