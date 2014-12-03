c397c01e72e8cbb1f15f9773f7a2dd60d5eca517
loic <loic.padier@gmail.com>
2014-12-04 00:59:05 +0100

    updated .gitignore to ignore pgm test directory, updated test to output to pgm test directory

--- src/pgm/core/Test.java
+++ src/pgm/core/Test.java
@@ -7,8 +7,9 @@ public class Test {
     public static void main(String[] args) {
 
         PGM pgm = null;
+        String name = "obama";
         try {
-            Loader loader = new Loader("feep.pgm");
+            Loader loader = new Loader("pgm/" + name + ".pgm");
             System.out.println("Loading file into PGM object");
             pgm = loader.loadPGM();
         } catch (Exception e) {
@@ -16,7 +17,12 @@ public class Test {
         }
 
         if (pgm != null) {
-            pgm.save("feep.copy.pgm");
+            int[] values = new int[] {50, 100, 250, 500,1000};
+            for (int value : values) {
+                pgm.boxResize(value, value).save("pgm_out/" + name + ".output." + value + ".box.pgm");
+                pgm.nearestNeighborResize(value,value).save("pgm_out/" + name + ".output." + value + ".nnr.pgm");
+            }
+
         }
 
         System.out.println("Done.");
