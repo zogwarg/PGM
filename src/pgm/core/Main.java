24c3dc95863845b729e1d264839109535d81b872
Thomas <thomas.durand.verjat@gmail.com>
2014-12-06 16:12:42 +0100

    useless comment removed

--- src/pgm/core/Main.java
+++ src/pgm/core/Main.java
@@ -5,9 +5,6 @@ import pgm.gui.ImageGUI;
 
 public class Main {
     public static void main(String[] args) {
-        // Menu GUI and Image GUI should be distinct, like GIMP
-
         GUI menus = new GUI();
     }
-
 }
